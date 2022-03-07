0fe752c2409a66b26122235a3c95fd37aeb86785
Cecil E. King <cek333@yahoo.com>
2022-03-06 23:35:23 -0500

    quick youtube test

---
+++ media_yt.js
@@ -0,0 +1,99 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/**
+ * Hardcoded media json objects
+ */
+const thumb_url = 'https://via.placeholder.com/225x127';
+let mediaJSON = {
+  'media': [{
+      'subtitle': 'Grumpy Bunny is grumpy',
+      'contentUrl': 'https://www.youtube.com/watch?v=X1U8mbFnOJA',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'Big Buck Bunny',
+      'duration': 596
+    },
+    {
+      'subtitle': 'Friends Proog and Emo journey inside the folds of a seemingly infinite Machine.',
+      'contentUrl': 'https://www.youtube.com/watch?v=5SQBaiNmdIU',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'Elephant Dream',
+      'duration': 653
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'https://www.youtube.com/watch?v=n-oPcVh8T7E',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'For Bigger Blazes',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'https://www.youtube.com/watch?v=9ogP9sumhxo',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'For Bigger Escape',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'https://www.youtube.com/watch?v=xjhhxFsStaM',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'For Bigger Fun',
+      'duration': 60
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'https://www.youtube.com/watch?v=ziBfOcYhJc0',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'For Bigger Joyrides',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'https://www.youtube.com/watch?v=tksuYYImCVc',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'For Bigger Meltdowns',
+      'duration': 15
+    },
+    {
+      'subtitle': 'A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.',
+      'contentUrl': 'https://www.youtube.com/watch?v=V0VfPlC9NjA',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'Sintel',
+      'duration': 887
+    },
+    {
+      'subtitle': 'In an apocalyptic future, a group of soldiers and scientists takes refuge in Amsterdam to try to stop an army of robots that threatens the planet.',
+      'contentUrl': 'https://www.youtube.com/watch?v=siXGkoQG5YE',
+      'contentType': 'video/mp4',
+      'thumb': thumb_url,
+      'title': 'Tears of Steel',
+      'duration': 743
+    }
+  ]
+};
+
+export {
+  mediaJSON
+}
